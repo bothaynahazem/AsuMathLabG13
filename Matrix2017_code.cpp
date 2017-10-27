d01b1075aa7391f357cf13511b465de519c345c4
Bothayna Hazem <bothayna.hazem@gmail.com>
2017-10-27 21:18:09 +0200

    Update Matrix2017_code.cpp

--- Matrix2017_code.cpp
+++ Matrix2017_code.cpp
@@ -17,104 +17,104 @@ Matrix::~Matrix() //default destructor
 }
 
 Matrix::Matrix(int nRows, int nColumns, int initialization, double initializationValue)
-{ 
+{
 	this->nRows = nRows;
 	this->nColumns = nColumns;
 	if ((nRows*nColumns) == 0)
-	{ 
+	{
 		values = NULL;
 		return;
 	}
 	values = new double*[nRows];
 	for (int iR = 0;iR<nRows;iR++)
-	{ 
+	{
 		values[iR] = new double[nColumns];
 		for (int iC = 0;iC<nColumns;iC++)
-		{ 
+		{
 			switch (initialization)
-			{ 
-			case MI_ZEROS: 
-				values[iR][iC] = 0; 
-				break; 
-			case MI_ONES: 
+			{
+			case MI_ZEROS:
+				values[iR][iC] = 0;
+				break;
+			case MI_ONES:
 				values[iR][iC] = 1;
 				break;
 			case MI_EYE:
-					values[iR][iC] = (iR == iC) ? 1 : 0;
-					break;
-			case MI_RAND: values[iR][iC] = (rand() % 1000000) / 1000000.0; 
-				break; 
+				values[iR][iC] = (iR == iC) ? 1 : 0;
+				break;
+			case MI_RAND: values[iR][iC] = (rand() % 1000000) / 1000000.0;
+				break;
 			case MI_VALUE:
-					values[iR][iC] = initializationValue;
-					break; 
-			} 
-		} 
-	} 
+				values[iR][iC] = initializationValue;
+				break;
+			}
+		}
+	}
 }
 
-Matrix::Matrix(int nRows, int nColumns, double first, ...) 
+Matrix::Matrix(int nRows, int nColumns, double first, ...)
 {
 	this->nRows = nRows;
 	this->nColumns = nColumns;
 	if ((nRows*nColumns) == 0)
-	{ 
+	{
 		values = NULL;
-		return; 
+		return;
 	}
 	values = new double*[nRows];
 	va_list va;
 	va_start(va, first);
-	for (int iR = 0;iR<nRows;iR++) 
-	{ 
+	for (int iR = 0;iR<nRows;iR++)
+	{
 		values[iR] = new double[nColumns];
 		for (int iC = 0;iC<nColumns;iC++)
-		{ 
+		{
 			values[iR][iC] = (iC == 0 && iR == 0) ? first : va_arg(va, double);
 		}
 	}
-	va_end(va); 
+	va_end(va);
 }
 
-Matrix::Matrix(Matrix& m) 
+Matrix::Matrix(Matrix& m)
 {
 	nRows = nColumns = 0;
 	values = NULL;
-	copy(m); 
+	copy(m);
 }
 
-Matrix::Matrix(string s) 
-{ 
+Matrix::Matrix(string s)
+{
 	nRows = nColumns = 0;
 	values = NULL;
 	copy(s);
 }
 
-void Matrix::copy(Matrix& m) 
-{ 
+Matrix::Matrix(double d)
+{
+	nRows = nColumns = 0;
+	values = NULL;
+	copy(d);
+}
+
+void Matrix::copy(Matrix& m)
+{
 	reset();
 	this->nRows = m.nRows;
 	this->nColumns = m.nColumns;
 	if ((nRows*nColumns) == 0)
-	{ 
+	{
 		values = NULL;
-		return; 
+		return;
 	}
 	values = new double*[nRows];
 	for (int iR = 0;iR<nRows;iR++)
-	{ 
+	{
 		values[iR] = new double[nColumns];
-		for (int iC = 0;iC<nColumns;iC++) 
-		{ 
+		for (int iC = 0;iC<nColumns;iC++)
+		{
 			values[iR][iC] = m.values[iR][iC];
 		}
-	} 
-}
-
-Matrix::Matrix(double d)
-{ 
-	nRows = nColumns = 0;
-	values = NULL;
-	copy(d);
+	}
 }
 
 void Matrix::copy(double d)
@@ -131,12 +131,12 @@ void Matrix::copy(string s)
 {
 	reset();
 	char* buffer = new char[s.length() + 1];
-	strcpy(buffer, s.c_str());
+	strcpy_s(buffer, s.length()+1, s.c_str());
 	char* lineContext;
 	char* lineSeparators = ";\r\n";
 	char* line = strtok_s(buffer, lineSeparators, &lineContext);
 	while (line)
-	{ 
+	{
 		Matrix row; char* context;char* separators = " []";
 		char* token = strtok_s(line, separators, &context);
 		while (token)
@@ -152,34 +152,37 @@ void Matrix::copy(string s)
 }
 
 void Matrix::reset()
-{ 
+{
 	if (values)
-	{ 
+	{
 		for (int i = 0;i<nRows;i++)
 			delete[] values[i];
-		delete[] values; 
+		delete[] values;
 	}
 	nRows = nColumns = 0;
 	values = NULL;
 }
 
-string Matrix::getString() 
-{ 
+string Matrix::getString()
+{
 	string s;
-	for (int iR = 0;iR<nRows;iR++)
-	{ 
-		for (int iC = 0;iC<nColumns;iC++)
-		{ 
+	for (int iR = 0; iR<nRows; iR++)
+	{
+		for (int iC = 0; iC<nColumns; iC++)
+		{
+			cout << values[iR][iC] << " ";
+			
 			char buffer[50];
-			sprintf_s(buffer, 50, "%g\t", values[iR][iC]);
+			//sprintf_s(buffer, 50, "%g\t", values[iR][iC]);
 			s += buffer;
 		}
+		cout<<endl;
 		s += "\n";
 	}
 	return s;
 }
 
-Matrix Matrix::operator=(Matrix& m) 
+Matrix Matrix::operator=(Matrix& m)
 {
 	copy(m);
 	return *this;
@@ -189,7 +192,7 @@ Matrix Matrix::operator=(double d) { copy(d);return *this; }
 Matrix Matrix::operator=(string s) { copy(s);return *this; }
 
 void Matrix::add(Matrix& m)
-{ 
+{
 	if (nRows != m.nRows || nColumns != m.nColumns)
 		throw("Invalid matrix dimension");
 	for (int iR = 0; iR < nRows; iR++)
@@ -203,7 +206,7 @@ void Matrix::operator+=(double d) { add(Matrix(nRows, nColumns, MI_VALUE, d)); }
 Matrix Matrix::operator+(Matrix& m) { Matrix r = *this;r += m;return r; }
 Matrix Matrix::operator+(double d) { Matrix r = *this;r += d;return r; }
 
-void Matrix::sub(Matrix& m) 
+void Matrix::sub(Matrix& m)
 {
 	if (nRows != m.nRows || nColumns != m.nColumns)
 		throw("Invalid matrix dimension");
@@ -219,10 +222,10 @@ void Matrix::operator-=(double d) { sub(Matrix(nRows, nColumns, MI_VALUE, d)); }
 Matrix Matrix::operator-(Matrix& m) { Matrix r = *this;r -= m;return r; }
 Matrix Matrix::operator-(double d) { Matrix r = *this;r -= d;return r; }
 
-void Matrix::mul(Matrix& m) 
+void Matrix::mul(Matrix& m)
 {
 	if (nRows != m.nRows || nColumns != m.nColumns)
-	throw("Invalid matrix dimension");
+		throw("Invalid matrix dimension");
 	Matrix r(nRows, m.nColumns);
 	for (int iR = 0;iR<r.nRows;iR++)
 	{
@@ -241,7 +244,7 @@ void Matrix::operator*=(double d)
 {
 	for (int iR = 0;iR<nRows;iR++)
 		for (int iC = 0;iC<nColumns;iC++)
-			values[iR][iC] *= 9;
+			values[iR][iC] *= d;
 }
 Matrix Matrix::operator*(Matrix& m) { Matrix r = *this;r *= m;return r; }
 Matrix Matrix::operator*(double d) { Matrix r = *this;r *= d;return r; }
@@ -263,21 +266,43 @@ Matrix Matrix::operator-()
 
 Matrix Matrix::operator+() { return *this; }
 
-void Matrix::setSubMatrix(int r, int c, Matrix& m) { if ((r + m.nRows)>nRows || (c + m.nColumns)>nColumns)throw("Invalid matrix dimension");for (int iR = 0;iR<m.nRows;iR++)for (int iC = 0;iC<m.nColumns;iC++)values[r + iR][c + iC] = m.values[iR][iC]; }Matrix Matrix::getSubMatrix(int r, int c, int nRows, int nColumns) { if ((r + nRows)>nRows || (c + nColumns)>nColumns)throw("Invalid matrix dimension");Matrix m(nRows, nColumns);for (int iR = 0;iR<m.nRows;iR++)for (int iC = 0;iC<m.nColumns;iC++)m.values[iR][iC] = values[r + iR][c + iC];return m; }
+void Matrix::setSubMatrix(int r, int c, Matrix& m)
+{
+	if ((r + m.nRows)>nRows || (c + m.nColumns)>nColumns)
+		throw("Invalid matrix dimension");
+	for (int iR = 0;iR<m.nRows;iR++)
+		for (int iC = 0;iC<m.nColumns;iC++)
+			values[r + iR][c + iC] = m.values[iR][iC];
+}
 
-void Matrix::addColumn(Matrix& m) 
+Matrix Matrix::getSubMatrix(int r, int c, int nRows, int nColumns)
+{
+	if ((r + nRows)>nRows || (c + nColumns)>nColumns)
+		throw("Invalid matrix dimension");
+	Matrix m(nRows, nColumns);
+	for (int iR = 0;iR<m.nRows;iR++)
+		for (int iC = 0;iC<m.nColumns;iC++)
+			m.values[iR][iC] = values[r + iR][c + iC];
+	return m;
+}
+
+/*void Matrix::setMatrix()
+{
+
+}*/
+void Matrix::addColumn(Matrix& m)
 {
 	Matrix n(max(nRows, m.nRows), nColumns + m.nColumns);
-	n.setMatrix(0, 0, *this);
-	n.setMatrix(0, nColumns, m);
+	n.setSubMatrix(0, 0, *this);
+	n.setSubMatrix(0, nColumns, m);
 	*this = n;
 }
 
-void Matrix::addRow(Matrix& m) 
+void Matrix::addRow(Matrix& m)
 {
 	Matrix n(nRows + m.nRows, max(nColumns, m.nColumns));
-	n.setMatrix(0, 0, *this);
-	n.setMatrix(nRows, 0, m);
+	n.setSubMatrix(0, 0, *this);
+	n.setSubMatrix(nRows, 0, m);
 	*this = n;
 }
 
@@ -300,7 +325,24 @@ double Matrix::getDeterminant()
 {
 	if (nRows != nColumns)
 		throw("Invalid matrix dimension");
-	if (nRows == 1 && nColumns == 1)return values[0][0];double value = 0, m = 1;for (int iR = 0;iR<nRows;iR++) { value += m * values[0][iR] * getCofactor(0, iR).getDeterminant(); m *= -1; }return value; }
+	if (nRows == 1 && nColumns == 1)return values[0][0];
+	double value = 0, m = 1;
+	for (int iR = 0;iR<nRows;iR++) 
+	{ 
+		value += m * values[0][iR] * getCofactor(0, iR).getDeterminant();
+	m *= -1; 
+	}
+	return value;
+}
 
-istream& operator >> (istream &is, Matrix& m) { string s;getline(is, s, ']');s += "]";m = Matrix(s);return is; }ostream& operator << (ostream &os, Matrix& m) { os << m.getString();return os; }
+istream& operator >> (istream &is, Matrix& m)
+{ 
+	string s;getline(is, s, ']');s += "]";
+	m = Matrix(s);return is;
+}
+ostream& operator << (ostream &os, Matrix& m)
+{ 
+	os << m.getString();
+	return os; 
+}
 
