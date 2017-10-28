9d6bf4e46b88e40216777c375ce0e0796510839b
Bothayna Hazem <bothayna.hazem@gmail.com>
2017-10-29 00:18:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
-	Matrix mymatrix("[1 0 0; 0 1 0; 0 0 1]");
-	mymatrix.getString();
+	Matrix mymatrix1("[25 25; 25 25]");
+	Matrix mymatrix2("[5 5; 1 25]");
+	Matrix mymatrix3;
+	mymatrix3=mymatrix1/mymatrix2;
+
+	cout<<mymatrix3;
 
 	int x;
-	cin >> x;
+//	cin >> x;
 	return 0;
 }
