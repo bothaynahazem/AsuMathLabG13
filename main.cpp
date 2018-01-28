6aaa1094f2893e614b617c53171a9cf8a6f4b3ca
IsmaelKhaled <31388232+IsmaelKhaled@users.noreply.github.com>
2018-01-28 20:14:41 +0200

    Phase 2 main function

--- main.cpp
+++ main.cpp
@@ -1,46 +1,77 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <math.h>
+#include <stack>
+#include <cstdio>
+#include <sstream>
 #include "Matrix2017.h"
+enum MI { MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE };
+using namespace std;
 
-#define INPUT_MATRICES_SIZE 100
-#define VARIABLES_NAMES_SIZE 100
+Matrix* input_matrices= new Matrix[100];// array to hold matrices values
+string* variables_names=new string[100];// array to hold matrices variable character
+int in_cntr=0;
+
+bool isChar(string s)
+{
+	//if (s.size() >1) return false;
+
+	switch (s[0]) {
+	case '+': return true;
+	case '-': return true;
+	case '*': return true;
+	case '/': return true;
+	case '(': return true;
+	case ')': return true;
+	case '^': return true;
+	case '.': if(s[1]=='+') return true;
+	else if(s[1]=='-') return true;
+    else if(s[1]=='*') return true;
+    else if(s[1]=='/') return true;
+    else if(s[1]=='^') return true;
+	default: return false;
+	}
+}
+
+bool isLetter(string s)
+{
+    if (s[0]>='A' && s[0]<='Z')
+        return true;
+    else
+        return false;
+}
+
+
 
 int main(int argc,char* argv[])
 {
-//    try
-//    {
-
-
-	char** commands;
-	 //ifstream infile("phase_1_example.txt");
-	 //ifstream infile("big_matrix_example.txt");
-	 ifstream infile(argv[1]);
-
-	Matrix* input_matrices= new Matrix[INPUT_MATRICES_SIZE];// array to hold matrices values
-	string* variables_names=new string[VARIABLES_NAMES_SIZE];// array to hold matrices variable charachter
-	//string variables_names=new string[60];// array to hold matrices variable charachter
-//	char* variables_names=new char[60];// array to hold matrices variable charachter
-
-	int in_cntr=0;// common index between 2 arrays
-
-	Matrix m;
-	string contents[50];//array contains file contents
-	string line;
-
-	int numberoflines=0;//index contains number of lines in file
-	int i=0;//index for contents array
-	//int max_number_of_characters_in_a_command=0;
-	int number_of_commands=0;
-
-    while(getline(infile,contents[i]))// getting file contents
+string matrix_char;
+ifstream infile(argv[1]);
+string contents[100];
+int numberoflines=0;
+int i=0;
+while(getline(infile,contents[i]))// getting file contents
     {
-        int pos =contents[i].find("[");
 
-        if(pos!=-1 && contents[i].find("]")==-1)
+        int pos =contents[i].find("[");
+        int nbracketso =0;
+        int nbracketsc =0;
+        for(int j=0;j<contents[i].length();j++)
         {
-			 getline(infile,contents[i+1],']');
-			 contents[i]+=contents[i+1];
+            if(contents[i][j]=='[')
+                nbracketso++;
+            else if(contents[i][j]==']')
+                nbracketsc++;
+        }
+        if(pos!=string::npos && nbracketso != nbracketsc)
+        {
+             getline(infile,contents[i+1]);
+             contents[i] = contents[i] + ";" + contents[i+1];
         }
 
-        if(contents[i]==";")
+
+        if(contents[i]=="\n"||contents[i] == "\r" || contents[i] == "\t"||contents[i].find_first_not_of(' ') == std::string::npos)
         {
             i--;
             numberoflines--;
@@ -49,292 +80,146 @@ int main(int argc,char* argv[])
         numberoflines++;
 
         i++;
-
     }
 
-	commands = new char*[numberoflines];
 
-    for (int j=0,jcopy=0;j<numberoflines+1;j++)
+	int pos1;
+for (i=0; i<numberoflines;i++)
+{
+    try
     {
-       int pos =contents[j].find("[");
-       if(pos!=-1)// "[" is found
-       {
-           variables_names[in_cntr]=contents[j].substr(0,1);// save the matrix variable char
-           //make it capital
-           line=contents[j].substr(pos);// getting only string with the matrix value
-           input_matrices[in_cntr]=line;// saving matrix value
-           in_cntr++;
-       }
-
-       else if (contents[j].find("=")!=-1)
-       {
-            int index =0;
-            for(int i=0;i<contents[j].length();i++)
-            {
-                if (contents[j].substr(i,1)==" " || contents[j].substr(i,1)==";")
-                    continue;
-                index ++;
-            }
-
-            commands[jcopy] = new char[(index-1)];
-            index =0;
-
-            for(int i=0;i<contents[j].length();i++)
-            {
-                if (contents[j].substr(i,1)==" " || contents[j].substr(i,1)==";")
-                continue;
-                commands[jcopy][index]=contents[j].at(i);
-                index++;
-            }
-
-            jcopy++;
-        }
-
-        number_of_commands=jcopy;
-    }
-
-    for (int i=0; i<number_of_commands; i++)
-    {
-        variables_names[i+2]=commands[i][0];  //i+2 --> because the first two are taken A & B
-    }
-
-if (variables_names[0]=="a")
-    variables_names[0]="A";
-
-Matrix first_matrix,second_matrix,result; //to carry out operations on
-
-string current_var_name;
-string first_operand; //for ex: A       /* why string ? because char didn't work as "variables_names is declared as string* and I don't know how to change it uptil now*/
-string second_operand; //for ex: B
-
-string test_var_name;
-string test_cmd_name;
-
-for (int a=0; a<number_of_commands; a++) //row
-    {
-        for (int b=0; b<(sizeof commands[i]/sizeof (char)); b++) //coloumn
+int openbrackets =0, closedbrackets=0;
+for (int h=0; h<contents[i].length();h++)
         {
-            if(commands[a][b]==commands[a][3]) // while iterating-->on finding the operation sign
+        if(contents[i][h]=='(')
+            openbrackets++;
+        else if(contents[i][h]==')')
+            closedbrackets++;
+        }
+    if (openbrackets!=closedbrackets)
+        {
+           throw("Syntax Error");
+        }
+	if (contents[i].find('[', 0) == string::npos && contents[i].find("ones", 0) == string::npos && contents[i].find("zeros", 0) == string::npos && contents[i].find("eye", 0) == string::npos && contents[i].find("rand", 0) == string::npos) //Only math expression
+	{
+	    string m;
+	    string e;
+		matrix_char = contents[i].substr(0, 1);
+		if (contents[i].find('=',0)!=string::npos)
+		{if (contents[i][contents[i].length()-1]!=';')
+			m = contents[i].substr(contents[i].find('=', 0)+1, contents[i].length() - contents[i].find('=', 0)-1);
+		else
+			m = contents[i].substr(contents[i].find('=', 0) + 1, contents[i].length() - contents[i].find('=', 0)-2);}
+        else{
+            m = contents[i].substr(0,1);
+        }
+        e = evaluate(m);
+        Matrix X(e);
+        variables_names[in_cntr] = matrix_char;
+        input_matrices[in_cntr] = X;
+        in_cntr++;
+	}
+	else if (contents[i].find('[', 0) != string::npos)
+	{
+	while (contents[i].find_first_of("+-*/^", contents[i].find('[', 0) + 1)!= string::npos) //find and replace all math expressions if square bracket is found
+	{
+		string d;
+		int begin, end;
+		pos1 = contents[i].find_first_of("+-*/^", contents[i].find('[', 0) + 1);
+		begin = contents[i].find_last_of(" [];", pos1);
+		end = contents[i].find_first_of(" [];", pos1);
+		d = contents[i].substr(begin+1, end-begin-1);
+		string f = evaluate(d);
+		if(f.find("-",0)!=string::npos)
+            f.replace(f.find("-",0),1,"$");
+		contents[i].replace(begin+1, end - begin-1, f);
+	}
+	while(contents[i].find('$',0)!=string::npos)
+        contents[i].replace(contents[i].find('$',0),1,"-");
+	matrix_char = contents[i].substr(0,1);
+	for (int k=1;k<contents[i].length();k++)
+    {
+        if(isLetter(contents[i].substr(k,1)))
+        {
+            for (int j=0;j<100;j++)
             {
-                switch(commands[a][3]) //switch on the operation (add+,subtract-,multiply*,divide/...etc)
+                if (contents[i].substr(k,1)==variables_names[j])
                 {
-                    case '+': //add
-                    {
-                        for(int c=0; c<number_of_commands; c++)
-                        {
-                            current_var_name=variables_names[c];
-                            first_operand=commands[a][2];
-                            second_operand=commands[a][4];
-
-                            if(current_var_name==first_operand)
-                                first_matrix=input_matrices[c];
-
-                            if (current_var_name==second_operand)
-                                second_matrix=input_matrices[c];
-                        }
-
-                        result=first_matrix+second_matrix;
-
-                        test_cmd_name=commands[a][0];
-                        for (int i=0; i<VARIABLES_NAMES_SIZE; i++)
-                        {
-                            //on finding the matrix to be modified, modify it then break
-                            test_var_name=variables_names[i];
-                            if (test_var_name==test_cmd_name)
-                            {
-                                input_matrices[i]=result;
-                                break;
-                            }
-                        }
-
-                        cout<< commands[a][0] << "=" << "\n" <<result <<"\n\n";
-                    }
-                    break;
-
-                    case '-': //subtract
-                    {
-
-                     for(int c=0; c<number_of_commands; c++)
-                        {
-                            current_var_name=variables_names[c];
-                            first_operand=commands[a][2];
-                            second_operand=commands[a][4];
-
-                            if(current_var_name==first_operand)
-                                first_matrix=input_matrices[c];
-
-                            if (current_var_name==second_operand)
-                                second_matrix=input_matrices[c];
-                        }
-                        result=first_matrix-second_matrix;
-
-                        test_cmd_name=commands[a][0];
-                        for (int i=0; i<VARIABLES_NAMES_SIZE; i++)
-                        {
-                            //on finding the matrix to be modified, modify it then break
-                            test_var_name=variables_names[i];
-                            if (test_var_name==test_cmd_name)
-                            {
-                                input_matrices[i]=result;
-                                break;
-                            }
-                        }
-
-                        cout<< commands[a][0] << "=" <<"\n" <<result <<"\n\n";
-                    }
-                    break;
-
-                    case '*':
-                    {
-                        for(int c=0; c<number_of_commands; c++)
-                        {
-                            current_var_name=variables_names[c];
-                            first_operand=commands[a][2];
-                            second_operand=commands[a][4];
-
-                            if(current_var_name==first_operand)
-                                first_matrix=input_matrices[c];
-
-                            if (current_var_name==second_operand)
-                                second_matrix=input_matrices[c];
-                        }
-                        result=first_matrix*second_matrix;
-
-                        test_cmd_name=commands[a][0];
-                        for (int i=0; i<VARIABLES_NAMES_SIZE; i++)
-                        {
-                            //on finding the matrix to be modified, modify it then break
-                            test_var_name=variables_names[i];
-                            if (test_var_name==test_cmd_name)
-                            {
-                                input_matrices[i]=result;
-                                break;
-                            }
-                        }
-
-                        cout<< commands[a][0] << "=" << "\n" <<result <<"\n\n";
-                    }
-                    break;
-
-                    case '/':
-                    {
-
-                    try{
-                        for(int c=0; c<number_of_commands; c++)
-                        {
-                            current_var_name=variables_names[c];
-                            first_operand=commands[a][2];
-                            second_operand=commands[a][4];
-
-                            if(current_var_name==first_operand)
-                                first_matrix=input_matrices[c];
-
-                            if (current_var_name==second_operand)
-                                second_matrix=input_matrices[c];
-                        }
-
-                        result=first_matrix/second_matrix;
-
-
-                        test_cmd_name=commands[a][0];
-                        for (int i=0; i<VARIABLES_NAMES_SIZE; i++)
-                        {
-                            //on finding the matrix to be modified, modify it then break
-                            test_var_name=variables_names[i];
-                            if (test_var_name==test_cmd_name)
-                            {
-                                input_matrices[i]=result;
-                                break;
-                            }
-                        }
-
-                        cout<< commands[a][0] << "=" <<"\n" <<result <<"\n\n";
-}
-                    catch(char const* error){cout<<"Error: "<<error<<"\n\n";}
-                    }
-                    break;
-
-                    /*
-                    this case could be split into another nested
-                    switch (or if statements)case for other operations
-                    starting with a period '.'
-                    */
-                    case '.':
-                    {
-                        if (commands[a][4] == '/') //rdivide operation
-                        {
-                            double inputDouble=0.0; //in case it's a double/Matrix
-                            bool MatOverMat=false; //not used but we may use it later
-
-                            for(int c=0; c<number_of_commands; c++)
-                            {
-                                current_var_name=variables_names[c];
-                                string current_double;
-                                current_double=commands[a][2];
-
-                                inputDouble=atof(current_double.c_str());
-                                first_operand=commands[a][5]; //the only operand
-
-                                if(current_var_name==first_operand)
-                                    first_matrix=input_matrices[c];
-                            }
-                             result=result.rdivide(inputDouble/*1 for example*/, first_matrix);
-                        }
-
-                            test_cmd_name=commands[a][0];
-                            for (int i=0; i<VARIABLES_NAMES_SIZE; i++)
-                            {
-                                //on finding the matrix to be modified, modify it then break
-                                test_var_name=variables_names[i];
-                                if (test_var_name==test_cmd_name)
-                                {
-                                    input_matrices[i]=result;
-                                    break;
-                                }
-                            }
-                            cout<< commands[a][0] << "=" << "\n" <<result <<"\n\n";
-                        }
-                        break;
-
-                    case '\'': //single quote --> Transpose
-                    {
-                        for(int c=0; c<number_of_commands; c++)
-                        {
-                            current_var_name=variables_names[c];
-                            first_operand=commands[a][2];
-
-                            if(current_var_name==first_operand)
-                                first_matrix=input_matrices[c];
-                        }
-                        result=first_matrix.getTranspose();
-
-                        test_cmd_name=commands[a][0];
-                        for (int i=0; i<VARIABLES_NAMES_SIZE; i++)
-                        {
-                            //on finding the matrix to be modified, modify it then break
-                            test_var_name=variables_names[i];
-                            if (test_var_name==test_cmd_name)
-                            {
-                                input_matrices[i]=result;
-                                break;
-                            }
-                        }
-                        cout<< commands[a][0] << "=" << "\n" <<result <<"\n\n";
-                    }
-                    break;
-
-                    default:
-                        cout<< "ERROR: Invalid Operation, please try again.";
+                    int opened_bracket = contents[i].find('[',k);
+                    int closed_bracket = contents[i].rfind(']',k);
+                    string space = contents[i].substr(k+1,opened_bracket-k-1);
+                    string space2 = contents[i].substr(closed_bracket+1,k-closed_bracket-1);
+                    if(space.find_first_not_of(' ') == std::string::npos || space2.find_first_not_of(' ') == std::string::npos)
+                    contents[i].replace(k,1,input_matrices[j].getAltString());
+                    else
+                     contents[i].replace(k,1,input_matrices[j].getAltStringNoB());
                     break;
                 }
+
             }
         }
     }
+	string x;
+    x = contents[i].substr(contents[i].find('[',0),contents[i].rfind(']')-contents[i].find('[',0)+1);
+    x = concatenate(x);
+    Matrix X(x);
+    input_matrices[in_cntr] = X;
+    variables_names[in_cntr] = matrix_char;
+    in_cntr++;
+	}
 
-//    }
-//
-//    catch (char* error){ cout<<"Error: "<<error<<endl; }
-	return 0;
+	else if (contents[i].find("rand", 0) != string::npos)
+	{
+	    matrix_char = contents[i].substr(0,1);
+		int nR = stoi(contents[i].substr(contents[i].find('(', 0) + 1, 1));
+		int nC = stoi(contents[i].substr(contents[i].find(',', 0) + 1, contents[i].find(')',0)-contents[i].find(',', 0) + 1));
+		Matrix X(nR, nC, MI_RAND,0.0);
+		variables_names[in_cntr] = matrix_char;
+        input_matrices[in_cntr] = X;
+        in_cntr++;
+	}
+	else if (contents[i].find("eye", 0) != string::npos)
+	{
+	    matrix_char = contents[i].substr(0,1);
+		int nR = stoi(contents[i].substr(contents[i].find('(', 0) + 1, 1));
+		int nC = stoi(contents[i].substr(contents[i].find(',', 0) + 1, contents[i].find(')',0)-contents[i].find(',', 0) + 1));
+		Matrix X(nR, nC, MI_EYE,0.0);
+		variables_names[in_cntr] = matrix_char;
+        input_matrices[in_cntr] = X;
+        in_cntr++;
+	}
+	else if (contents[i].find("zeros", 0) != string::npos)
+	{
+	    matrix_char = contents[i].substr(0,1);
+		int nR = stoi(contents[i].substr(contents[i].find('(', 0) + 1, 1));
+		int nC = stoi(contents[i].substr(contents[i].find(',', 0) + 1, contents[i].find(')',0)-contents[i].find(',', 0) + 1));
+		Matrix X(nR, nC, MI_ZEROS,0.0);
+		variables_names[in_cntr] = matrix_char;
+        input_matrices[in_cntr] = X;
+        in_cntr++;
+	}
+	else if (contents[i].find("ones", 0) != string::npos)
+	{
+	    matrix_char = contents[i].substr(0,1);
+		int nR = stoi(contents[i].substr(contents[i].find('(', 0) + 1, 1));
+		int nC = stoi(contents[i].substr(contents[i].find(',', 0) + 1, contents[i].find(')',0)-contents[i].find(',', 0) + 1));
+		Matrix X(nR, nC, MI_ONES,0.0);
+
+        variables_names[in_cntr] = matrix_char;
+        input_matrices[in_cntr] = X;
+        in_cntr++;
+	}
+
+	//Output all the results
+if(contents[i][contents[i].length()-2]!=';') //might be length()-1 due to difference in compilers ('\n') is counted in some compilers
+cout << variables_names[in_cntr-1] << " = " << endl << input_matrices[in_cntr-1] << endl;
+    }
+    catch(char const* s)
+    {
+        cout<< "Error: " << s << endl;
+    }
+	}
+
+
+return 0;
 }
-
-
